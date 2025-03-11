e6aea0520bec9aa0cbd97bb289a97a8b179f492a
chiheb rahmouni <chihebrahmouni30@gmail.com>
2025-03-11 18:18:49 +0000

    Refactor search functionality and implement React Query

--- src/containers/boats/BoatsCont.tsx
+++ src/containers/boats/BoatsCont.tsx
@@ -2,83 +2,68 @@ import ShipDetails from "../../components/boats/ShipDetailsComp";
 import { useEffect, useState } from "react";
 import LoadingLine from "../../components/ui/LoadingLine";
 import axios from "axios";
-import { useNavigate, useLocation } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import SearchBarsCont from "./SearchBarsCont";
 import { Pagination } from "@mui/material";
-import {
-  axios_error_handler
-} from "../../functions/axios_error_handler";
+import { axios_error_handler } from "../../functions/axios_error_handler";
+import { useQuery } from "@tanstack/react-query";
 
-
-const BoatsCont = () => {
-  const [shipsArray, setShipsArray] = useState<any>([]);
-  const [loading, setLoading] = useState(true);
-  const [totalPages, setTotalPages] = useState(1);
-  const [currentPage, setCurrentPage] = useState(1);
-  const navigate = useNavigate();
-  const location = useLocation();
-  const { t } = useTranslation();
+const fetchShips = async (
+  currentPage: number,
+  boatName: string,
+  ownerName: string
+) => {
+  const params = new URLSearchParams();
+  params.append("page", currentPage.toString());
+  if (boatName) params.append("boatName", boatName);
+  if (ownerName) params.append("ownerName", ownerName);
 
   const url = import.meta.env.VITE_SERVER_URL_LISTING;
-  const fetchData = (currentPage: number) => {
-    axios
-      .get(`${url}/api/listing/listings?page=${currentPage}`)
-      .then((response) => {
-        setShipsArray(response.data.listings);
-        setTotalPages(response.data.pagination.totalPages);
-        setLoading(false);
-      })
-      .catch((error: any) => {
-        setLoading(false);
-        axios_error_handler(error, t);
-      });
-  };
+  const response = await axios.get(`${url}/api/listing/listings?${params}`);
+  return response.data;
+};
+
+const BoatsCont = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const { t } = useTranslation();
+  const [boatName, setBoatName] = useState("");
+  const [ownerName, setOwnerName] = useState("");
+
+  const { data, isLoading, error, refetch } = useQuery({
+    queryKey: ["ships", currentPage],
+    queryFn: () => fetchShips(currentPage, boatName, ownerName),
+  });
 
   useEffect(() => {
-    const query = new URLSearchParams(location.search);
-    const page = query.get("page");
-    if (page) {
-      const pageNumber = Number(page);
-      if (pageNumber > 0 && pageNumber <= totalPages) {
-        setCurrentPage(pageNumber);
-      } else {
-        navigate(`?page=1`, { replace: true });
-      }
-    } else {
-      navigate(`?page=${currentPage}`, { replace: true });
-    }
-  }, [location.search, totalPages, navigate]);
+    if (error) axios_error_handler(error, t);
+  }, [error]);
+  if (error) return null;
 
-  useEffect(() => {
-    fetchData(currentPage);
-  }, [currentPage]);
-
-  useEffect(() => {
-    navigate(`?page=${currentPage}`, { replace: true });
-  }, [currentPage, navigate]);
+  if (isLoading) return <LoadingLine />;
 
   return (
     <>
       <SearchBarsCont
-        setLoading={setLoading}
-        setShipsArray={setShipsArray}
-        setTotalPages={setTotalPages}
+        boatName={boatName}
+        setBoatName={setBoatName}
+        ownerName={ownerName}
+        setOwnerName={setOwnerName}
+        refetch={refetch}
       />
       <div className="w-full grid mt-[65px] grid-cols-1 justify-items-center gap-y-16 md:grid-cols-2 md:gap-x-4 lg:grid-cols-3 xl:grid-cols-4 lg:gap-y-16 lg:gap-x-4 2xl:grid-cols-4">
-        {loading ? (
+        {isLoading ? (
           <div className="col-span-1 w-[100%] h-40 justify-center items-center md:col-span-2 lg:col-span-3 xl:col-span-4">
             <LoadingLine />
           </div>
         ) : (
-          shipsArray.map((ship: any, index: number) => (
+          data.listings.map((ship: any, index: number) => (
             <ShipDetails key={index} ship={ship} />
           ))
         )}
       </div>
       <div className="pagination w-full mt-10 flex justify-center">
         <Pagination
-          count={totalPages}
+          count={data.pagination.totalPages}
           page={currentPage}
           onChange={(_event, value) => {
             setCurrentPage(value);
@@ -90,4 +75,3 @@ const BoatsCont = () => {
 };
 
 export default BoatsCont;
-
--- src/containers/boats/SearchBarsCont.tsx
+++ src/containers/boats/SearchBarsCont.tsx
@@ -1,67 +1,44 @@
 import { useTranslation } from 'react-i18next'
-import axios from 'axios'
-import { useState, useCallback } from 'react'
-import { axios_error_handler } from '../../functions/axios_error_handler'
 import InputSearch from '../../components/ui/inputs/InputSearch'
 
 
 interface Props {
-  setLoading: any;
-  setShipsArray: any;
-  setTotalPages: any;
+  boatName: string;
+  setBoatName: any;
+  ownerName: string;
+  setOwnerName: any;
+  refetch: any;
 }
 
 
 const SearchBarsCont = ({
-  setLoading,
-  setShipsArray,
-  setTotalPages
-
+  boatName,
+  setBoatName,
+  ownerName,
+  setOwnerName,
+refetch
 }: Props) => {
 
 
   const { t, i18n } = useTranslation();
-  const url = import.meta.env.VITE_SERVER_URL_LISTING;
-    const [boatSearch, setBoatSearch] = useState("");
-    const [ownerSearch, setOwnerSearch] = useState("");
 
-
-  const search_by_boat_name = useCallback(() => {
-    // console.log("boat");
-    if (boatSearch === "") return;
-    setLoading(true);
-    // /api/listing/listings/search?boatName=Ocean&ownerName=sssss&page=2&limit=5
-    axios
-      .get(`${url}/api/listing/listings?boatName=${boatSearch}`)
-      .then((response) => {
-        setShipsArray(response.data.listings);
-        setTotalPages(response.data.pagination.totalPages);
-        setLoading(false);
-      })
-      .catch((error) => {
-        setLoading(false);
-        axios_error_handler(error, t);
-      });
-  }, [boatSearch]);
-
-  const search_by_owner_name = useCallback(() => {
-    console.log(ownerSearch);
-  }, [ownerSearch]);
 
   return (
     <div className="w-full flex flex-col md:flex-row gap-4 max-w-[1000px] mx-auto">
       <InputSearch
-        value={boatSearch}
-        setValue={(e: any) => setBoatSearch(e.target.value)}
+        value={boatName}
+        setValue={(e: any) => setBoatName(e.target.value)}
         label={t("search_by_boat_name") + "..."}
-        onClick={search_by_boat_name}
+        onClick={refetch}
+        // onClick={() => console.log("search_by_boat_name")}
         i18n={i18n}
       />
       <InputSearch
-        value={ownerSearch}
-        setValue={(e: any) => setOwnerSearch(e.target.value)}
+        value={ownerName}
+        setValue={(e: any) => setOwnerName(e.target.value)}
         label={t("search_by_owner_name") + "..."}
-        onClick={search_by_owner_name}
+        onClick={refetch}
+        // onClick={() => console.log("search_by_owner_name")}
         i18n={i18n}
       />
     </div>
